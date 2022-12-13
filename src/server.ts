d78a310ec084bc7af97691ddf2039cb526c962b7
Vladyslav Kukharchuk <vladyslavziuziun@gmail.com>
2022-12-13 19:58:58 +0200

    functions now return a 200 response code

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,36 @@ import express from 'express';
 const app = express();
 const httpServerPort = 3000;
 
-app.get('/', (req, res) => {
+// HTTP
+// логин(получаем email и пароль, находим юзера, проверяем и возвращаем jwt токен)
+
+app.post('/users/login', (req, res) => {
     res.sendStatus(200);
-    // res.send("Hello, this is Http Server, I'm alive!");
-})
+});
+
+
+// регистрация(передаем ник, email, пароль, дубль пароля, id выбранного класса)
+
+app.post('/users/new', (req, res) => {
+    res.sendStatus(200);
+});
+
+
+// обновление личных данных(ник, старый пароль, пароль, дубль пароля, id нового класса)
+
+app.patch('/users/:userid', (req, res) => {
+    res.sendStatus(200);
+});
+
+
+// получение списка классов.
+
+app.get('/classes', (req, res) => {
+    res.sendStatus(200);
+});
+
+// forgot password или подтверждение email не требуется.
+
 
 app.listen(httpServerPort, () => {
     console.log(`Http Server started on port ${httpServerPort}.`)
@@ -22,16 +48,41 @@ const wsServerPort = 3001;
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-wss.on('connection', (ws: WebSocket) => {
-
-    ws.send("Hello, this is WebSocket Server, I'm alive!");
-
-    ws.on('message', (message: string) => {
-        ws.send(`Hello, you sent -> ${message}`);
-    });
-  
-});
-
 server.listen(wsServerPort, () => {
     console.log(`WebSocket Server started on port ${wsServerPort}.`);
+});
+
+// WS
+
+wss.on('connection', (ws: WebSocket) => {
+
+    // получать и различать события(удар, применение способности, сообщение, возрождение)
+    // удар, применение способности и возрождение должны передаваться с id целевого юзера.
+
+    // {
+    //     "event": "message",
+    //     "id": "1"
+    // }
+
+    ws.on('message', (message: string) => {
+
+        let data = (JSON.parse(message));
+
+        switch (data.event) {
+            case "attack":
+                ws.send(`attack ${data.id}`);
+                break;
+            case "ability":
+                ws.send(`ability ${data.id}`);
+                break;
+            case "relive":
+                ws.send(`relive ${data.id}`);
+                break;
+            case "message":
+                ws.send("message");
+                break;
+        }
+
+        ws.send(`Hello, you sent -> ${message}`);
+    });
 });
